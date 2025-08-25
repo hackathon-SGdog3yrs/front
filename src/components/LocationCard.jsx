364e772d2c023c92d50f4b9ad763dd8fc20226a2
김현중 <ryankim00@gimhyeonjung-ui-MacBookAir.local>
2025-08-25 22:23:33 +0900

    작업 반영

--- src/components/LocationCard.jsx
+++ src/components/LocationCard.jsx
@@ -3,6 +3,7 @@ import React from 'react';
 import ImageSwiper from './ImageSwiper';
 import '../styles/LocationCard.css';
 import LocationIcon from '../assets/default.svg';
+import AdIcon from '../assets/adicon.svg'
 
 const LocationCard = ({ location, onClick }) => {
     return (
@@ -10,7 +11,16 @@ const LocationCard = ({ location, onClick }) => {
         <div className="location-card" onClick={onClick}>
             <ImageSwiper images={location.picture} />
             <div className="card-details">
-                <p className="card-name">{location.name}</p>
+                <div className = "card-info">
+                    <p className="card-name">{location.name}</p>
+                    {location?.advertisement === true && (
+                        <div className="ad">
+                            <span className="card-ad">광고</span>
+                            <img src={AdIcon} alt="광고 아이콘" className="ad-icon" />
+                        </div>
+                    )}
+
+                </div>
                 <div className="card-meta-loc">
                     <img src={LocationIcon} alt="위치 아이콘" className="meta-icon" />
                     <p>{location.address}</p>
